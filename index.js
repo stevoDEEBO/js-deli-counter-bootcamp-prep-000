f073cfb0a4a7cb44f15ee6c6c1a0af7d24dd1c0e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-26 04:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing (katzDeliLine) {
   else {
     return "Currently serving " + katzDeliLine[0] + "."
   }
-  return katzDeliLine.shift()
+  return katzDeliLine.slice(1)
 }
 
 function currentLine (katzDeliLine) {
