8a9d333f0f4f564fb5027a5e994599026d2b162f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-26 04:08:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var katzDeli = []
+
+function takeANumber (katzDeliLine, name) {
+  var katzDeli = katzDeliLine.push(name)
+  const number = katzDeliLine.length
+  return "Welcome, " + name + ". You are number " + number + " in line."
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  
+  else {
+    return "Currently serving " + katzDeliLine[0] + "."
+    katzDeliLine.shift()
+    return katzDeliLine
+  }
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  
+  else {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      const currList = []
+      var person = String(katzDeliLine.indexOf(i) + ". " + katzDeliLine[i]) + ", "
+      currList.push(person)
+      return "The line is currently: " + currList
+    }
+  }
+}
