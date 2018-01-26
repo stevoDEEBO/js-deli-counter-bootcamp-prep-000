7f45ac04cef2dc4784e3810f26291aaf4425d2a8
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-26 04:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ function currentLine (katzDeliLine) {
     const currList = []
     let i = 0
     while (i < katzDeliLine.length) {
-      currList.push(i+1 + ". " + katzDeliLine[i]) + " "
+      currList.push(" " + i+1 + ". " + katzDeliLine[i])
       i++
     }
-    return "The line is currently: " + currList
+    return "The line is currently:" + currList
   }
 }
